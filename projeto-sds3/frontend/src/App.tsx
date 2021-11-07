9e33b49d8d491786c156ef27cd7b1b8e11a83aee
neitondurval <neitondurval@gmail.com>
2021-11-07 01:28:03 -0300

    Static charts

--- projeto-sds3/frontend/src/App.tsx
+++ projeto-sds3/frontend/src/App.tsx
@@ -1,8 +1,40 @@
+import BarChart from "components/BarChart/Index";
+import DataTable from "components/DataTable/Index";
+import DonutChart from "components/DonutChart/Index";
+import Footer from "components/Footer/Index";
+import NavBar from "components/NavBar/Index";
+
 function App() {
   return (
-    <div className="App">
-      <h1>Olá Mundo</h1>
-    </div>
+    <>
+      <NavBar />
+      <div className="container">
+        <h1 className="text-primary py-3">Dashboard de Vendas</h1>
+        <div className="row px-3">
+              <div className="col-sm-6">
+                  <h5 className="text-center text-secondary">Taxa de Sucesso(%)</h5>
+                  <BarChart />
+
+              </div>
+
+          <div className="row px-3">
+              <div className="col-sm-6">
+              <h5 className="text-center text-secondary">Todas as Vendas</h5>
+                  <DonutChart />
+
+              </div>
+
+          </div>
+          
+          </div>
+          <div className="py-3">
+            <h2 className="text-primary">Todas Vendas </h2>
+          </div>
+
+        <DataTable />
+      </div>
+    <Footer />
+    </>
   );
 }
 
